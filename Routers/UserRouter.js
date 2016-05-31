79a3fa46ca3b70eee1fd25b02c34cdef005de4d7
teresa lee <teresa@teresas-MacBook-Pro.local>
2016-05-31 10:09:03 -0400

    feature(userRouter) add GET all

--- Routers/UserRouter.js
+++ Routers/UserRouter.js
@@ -3,4 +3,16 @@ import * as Services from '../Services/Service';
 
 const UserRouter = new Router();
 
+// GET All Users
+UserRouter.get('/users', async function () {
+  try {
+    const result = await Services.getAllUsers();
+
+    this.body = result;
+  } catch (error) {
+    this.status = 400;
+    this.body = 'There was a problem fetching users';
+  }
+});
+
 export default UserRouter;
--- Services/Service.js
+++ Services/Service.js
@@ -6,3 +6,8 @@ const MongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/hw';
 
 mongorito.connect(MongoUrl);
 console.log(`Connected to DB ${MongoUrl}`);
+
+// USER(S)
+export function getAllUsers () {
+  return User.sort('created_at', -1).find();
+}
